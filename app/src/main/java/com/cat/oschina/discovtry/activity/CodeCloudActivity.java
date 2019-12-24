25404fb84e6f52877bf9425f3cd7b536673b99da
279659746 <2796859746@qq.com>
2019-12-24 15:02:27 +0800

    整个项目的综合

--- app/src/main/java/com/cat/oschina/discovtry/activity/CodeCloudActivity.java
+++ app/src/main/java/com/cat/oschina/discovtry/activity/CodeCloudActivity.java
@@ -21,7 +21,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class CodeCloudActivity extends AppCompatActivity implements View.OnClickListener {
-
+//
     private Unbinder binder;
 
     @BindView(R.id.recyclerView_codecloud)
